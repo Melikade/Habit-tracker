82c2b802147405343e8ca6bb08a5b7cb7c25805d
mitrape <154874223+mitrape@users.noreply.github.com>
2024-03-13 12:08:58 +0330

    Update Task package

--- TaskPackage/Task.java
+++ TaskPackage/Task.java
@@ -1,2 +1,13 @@
-package TaskPackage;public class Task {
+package TaskPackage;
+
+public class Task {
+    public double start_time=0;
+    public double finish_time=0;
+    public String name;
+    public Task(String name){
+        this.name = name;
+    }
+    public double getDuration(){
+        return(finish_time-start_time);
+    }
 }
